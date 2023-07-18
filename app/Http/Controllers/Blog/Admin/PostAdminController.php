aa718747114f827eae92967e15271b697c651a66
Bortnyk Rostylav <bortnikrostislav370@gmail.com>
2023-07-18 19:50:51 +0300

    Blog post update

--- app/Http/Controllers/Blog/Admin/PostAdminController.php
+++ app/Http/Controllers/Blog/Admin/PostAdminController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers\Blog\Admin;
 
+use Carbon\Carbon;
 use Illuminate\Support\Str;
+use Illuminate\Http\Request;
 use App\Models\BlogPost;
 use Illuminate\Support\Facades\View;
 use App\Repositories\BlogPostRepository;
 use App\Repositories\BlogCategoryRepository;
+use App\Http\Requests\BlogCategoryUpdateRequest;
+use App\Http\Requests\BlogCategoryCreateRequest;
+use App\Http\Requests\BlogPostUpdateRequest;
+use App\Http\Requests\BlogPostCreateRequest;
 
 /**
  * Управління статтями блога
@@ -74,9 +80,36 @@ class PostAdminController extends BaseController
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(BlogPostUpdateRequest $request, string $id)
     {
-        dd(__METHOD__, $request->all(), $id);
+        $item = $this->blogPostRepository->getEdit($id);
+
+        if(empty($item)){
+            return back()
+                ->withErrors(['msg' => 'Запмис id=[{id}] не знайдено'])
+                ->withInput();
+        }
+
+        $data = $request->all();
+
+        if(empty($data['slug'])){
+            $data['slug'] = Str::slug($data['title']);
+        }
+        if(empty($item->published_at) && $data['is_published']){
+            $data['published_at'] = Carbon::now();
+        }
+
+        $result = $item->update($data);
+
+        if($result){
+            return redirect()
+                ->route('blog.admin.posts.edit', $item->id)
+                ->with(['success' => 'Успішне виконання']);
+        } else{
+            return back()
+                ->withErrors(['msg' => 'Помилка збереження'])
+                ->withInput();
+        }
     }
 
     /**
